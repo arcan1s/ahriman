5815928b7f06cffd5e3790b8819e9f6eb290b786
Evgeniy Alekseev <i@arcanis.me>
2021-03-08 15:59:41 +0300

    update release creation script

--- make_release.sh
+++ make_release.sh
@@ -11,25 +11,25 @@ IGNORELIST="build .idea package/archlinux package/*src.tar.xz"
 sed -i "/__version__ = '[0-9.]*/s/[^'][^)]*/__version__ = '$VERSION'/" src/ahriman/version.py
 
 # create archive
-[[ -e ${ARCHIVE}-${VERSION}-src.tar.xz ]] && rm -f "${ARCHIVE}-${VERSION}-src.tar.xz"
+[[ -e $ARCHIVE-$VERSION-src.tar.xz ]] && rm -f "$ARCHIVE-$VERSION-src.tar.xz"
 [[ -d $ARCHIVE ]] && rm -rf "$ARCHIVE"
 mkdir "$ARCHIVE"
 for FILE in ${FILES[*]}; do cp -r "$FILE" "$ARCHIVE"; done
-for FILE in ${IGNORELIST[*]}; do rm -rf "${ARCHIVE}/${FILE}"; done
-tar cJf "${ARCHIVE}-${VERSION}-src.tar.xz" "$ARCHIVE"
+for FILE in ${IGNORELIST[*]}; do rm -rf "$ARCHIVE/$FILE"; done
+tar cJf "$ARCHIVE-$VERSION-src.tar.xz" "$ARCHIVE"
 rm -rf "$ARCHIVE"
 
 # update checksums
-SHA512SUMS=$(sha512sum ${ARCHIVE}-${VERSION}-src.tar.xz | awk '{print $1}')
+SHA512SUMS=$(sha512sum $ARCHIVE-$VERSION-src.tar.xz | awk '{print $1}')
 sed -i "/sha512sums=('[0-9A-Fa-f]*/s/[^'][^)]*/sha512sums=('$SHA512SUMS'/" package/archlinux/PKGBUILD
 sed -i "s/pkgver=[0-9.]*/pkgver=$VERSION/" package/archlinux/PKGBUILD
 
 # clear
-find . -type f -name '*src.tar.xz' -not -name "*${VERSION}-src.tar.xz" -exec rm -rf {} \;
+find . -type f -name '*src.tar.xz' -not -name "*$VERSION-src.tar.xz" -exec rm -f {} \;
 
-exit 0
-
-# tag
-git add package/archlinux/PKGBUILD
-git commit -m "Release $VERSION" && git push
-git tag $VERSION && git push --tags
+read -p "Publish release? [Ny] " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git add package/archlinux/PKGBUILD
+  git commit -m "Release $VERSION" && git push
+  git tag "$VERSION" && git push --tags
+fi
