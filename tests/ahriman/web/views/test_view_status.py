a37cb72d4e24f848b2cf1cd78006cbe96ab6928b
Evgeniy Alekseev <i@arcanis.me>
2021-04-07 01:19:13 +0300

    add status route

---
+++ tests/ahriman/web/views/test_view_status.py
@@ -0,0 +1,22 @@
+from pytest_aiohttp import TestClient
+
+import ahriman.version as version
+
+from ahriman.models.build_status import BuildStatusEnum
+from ahriman.models.package import Package
+
+
+async def test_get(client: TestClient, package_ahriman: Package) -> None:
+    """
+    must generate web service status correctly)
+    """
+    await client.post(f"/api/v1/packages/{package_ahriman.base}",
+                      json={"status": BuildStatusEnum.Success.value, "package": package_ahriman.view()})
+
+    response = await client.get("/api/v1/status")
+    assert response.status == 200
+
+    json = await response.json()
+    assert json["version"] == version.__version__
+    assert json["packages"]
+    assert json["packages"]["total"] == 1
