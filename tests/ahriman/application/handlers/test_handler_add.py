6551c8d9832657245bb009fc5516bd1e3468fb5c
Evgeniy Alekseev <i@arcanis.me>
2022-04-10 01:51:12 +0300

    merge restore to rebuild commannd

--- tests/ahriman/application/handlers/test_handler_add.py
+++ tests/ahriman/application/handlers/test_handler_add.py
@@ -3,7 +3,6 @@ import pytest
 
 from pytest_mock import MockerFixture
 
-from ahriman.application.application import Application
 from ahriman.application.handlers import Add
 from ahriman.core.configuration import Configuration
 from ahriman.models.package import Package
@@ -35,20 +34,6 @@ def test_run(args: argparse.Namespace, configuration: Configuration, mocker: Moc
 
     Add.run(args, "x86_64", configuration, True, False)
     application_mock.assert_called_once_with(args.package, args.source, args.without_dependencies)
-
-
-def test_run_extract_packages(args: argparse.Namespace, configuration: Configuration, mocker: MockerFixture) -> None:
-    """
-    must run command
-    """
-    args = _default_args(args)
-    args.package = None
-    mocker.patch("ahriman.models.repository_paths.RepositoryPaths.tree_create")
-    mocker.patch("ahriman.application.application.Application.add")
-    extract_mock = mocker.patch("ahriman.application.handlers.Add.extract_packages", return_value=[])
-
-    Add.run(args, "x86_64", configuration, True, False)
-    extract_mock.assert_called_once_with(pytest.helpers.anyvar(int))
 
 
 def test_run_with_updates(args: argparse.Namespace, configuration: Configuration,
@@ -87,12 +72,3 @@ def test_run_empty_exception(args: argparse.Namespace, configuration: Configurat
 
     Add.run(args, "x86_64", configuration, True, False)
     check_mock.assert_called_once_with(True, True)
-
-
-def test_extract_packages(application: Application, mocker: MockerFixture) -> None:
-    """
-    must extract packages from database
-    """
-    packages_mock = mocker.patch("ahriman.core.database.sqlite.SQLite.packages_get")
-    Add.extract_packages(application)
-    packages_mock.assert_called_once_with()
--- tests/ahriman/application/handlers/test_handler_rebuild.py
+++ tests/ahriman/application/handlers/test_handler_rebuild.py
@@ -4,6 +4,7 @@ import pytest
 from pytest_mock import MockerFixture
 from unittest import mock
 
+from ahriman.application.application import Application
 from ahriman.application.handlers import Rebuild
 from ahriman.core.configuration import Configuration
 from ahriman.models.package import Package
@@ -18,6 +19,7 @@ def _default_args(args: argparse.Namespace) -> argparse.Namespace:
     """
     args.depends_on = []
     args.dry_run = False
+    args.from_database = False
     args.exit_code = False
     return args
 
@@ -40,6 +42,20 @@ def test_run(args: argparse.Namespace, package_ahriman: Package,
     application_packages_mock.assert_called_once_with(None)
     application_mock.assert_called_once_with([package_ahriman])
     check_mock.assert_has_calls([mock.call(False, False), mock.call(False, False)])
+
+
+def test_run_extract_packages(args: argparse.Namespace, configuration: Configuration, mocker: MockerFixture) -> None:
+    """
+    must run command
+    """
+    args = _default_args(args)
+    args.from_database = True
+    mocker.patch("ahriman.models.repository_paths.RepositoryPaths.tree_create")
+    mocker.patch("ahriman.application.application.Application.add")
+    extract_mock = mocker.patch("ahriman.application.handlers.Rebuild.extract_packages", return_value=[])
+
+    Rebuild.run(args, "x86_64", configuration, True, False)
+    extract_mock.assert_called_once_with(pytest.helpers.anyvar(int))
 
 
 def test_run_dry_run(args: argparse.Namespace, configuration: Configuration,
@@ -116,3 +132,12 @@ def test_run_build_empty_exception(args: argparse.Namespace, configuration: Conf
 
     Rebuild.run(args, "x86_64", configuration, True, False)
     check_mock.assert_has_calls([mock.call(True, False), mock.call(True, True)])
+
+
+def test_extract_packages(application: Application, mocker: MockerFixture) -> None:
+    """
+    must extract packages from database
+    """
+    packages_mock = mocker.patch("ahriman.core.database.sqlite.SQLite.packages_get")
+    Rebuild.extract_packages(application)
+    packages_mock.assert_called_once_with()
