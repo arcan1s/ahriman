arcan1s/ahriman

commit d81e91e1174a8854123c4ae26b6152fb3e24682d

execute request in context methods instead of handling them each time manually

src/ahriman/core/status/web_client.py
@@ -17,10 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+import contextlib
 import logging
 import requests
 
-from typing import List, Optional, Tuple
+from typing import Generator, List, Optional, Tuple
 from urllib.parse import quote_plus as urlencode
 
 from ahriman.core.configuration import Configuration
@@ -98,6 +99,18 @@ class WebClient(Client, LazyLogging):
             address = f"http://{host}:{port}"
         return address, False
 
+    @contextlib.contextmanager
+    def __execute_request(self) -> Generator[None, None, None]:
+        """
+        execute request and handle exceptions
+        """
+        try:
+            yield
+        except requests.HTTPError as e:
+            self.logger.exception("could not perform http request: %s", exception_response_text(e))
+        except Exception:
+            self.logger.exception("could not perform http request")
+
     def _create_session(self, *, use_unix_socket: bool) -> requests.Session:
         """
         generate new request session
@@ -130,13 +143,9 @@ class WebClient(Client, LazyLogging):
             "password": self.user.password
         }
 
-        try:
+        with self.__execute_request():
             response = self.__session.post(self._login_url, json=payload)
             response.raise_for_status()
-        except requests.HTTPError as e:
-            self.logger.exception("could not login as %s: %s", self.user, exception_response_text(e))
-        except Exception:
-            self.logger.exception("could not login as %s", self.user)
 
     def _logs_url(self, package_base: str) -> str:
         """
@@ -177,13 +186,9 @@ class WebClient(Client, LazyLogging):
             "package": package.view()
         }
 
-        try:
+        with self.__execute_request():
             response = self.__session.post(self._package_url(package.base), json=payload)
             response.raise_for_status()
-        except requests.HTTPError as e:
-            self.logger.exception("could not add %s: %s", package.base, exception_response_text(e))
-        except Exception:
-            self.logger.exception("could not add %s", package.base)
 
     def get(self, package_base: Optional[str]) -> List[Tuple[Package, BuildStatus]]:
         """
@@ -195,7 +200,7 @@ class WebClient(Client, LazyLogging):
         Returns:
             List[Tuple[Package, BuildStatus]]: list of current package description and status if it has been found
         """
-        try:
+        with self.__execute_request():
             response = self.__session.get(self._package_url(package_base or ""))
             response.raise_for_status()
 
@@ -204,10 +209,8 @@ class WebClient(Client, LazyLogging):
                 (Package.from_json(package["package"]), BuildStatus.from_json(package["status"]))
                 for package in status_json
             ]
-        except requests.HTTPError as e:
-            self.logger.exception("could not get %s: %s", package_base, exception_response_text(e))
-        except Exception:
-            self.logger.exception("could not get %s", package_base)
+
+        # noinspection PyUnreachableCode
         return []
 
     def get_internal(self) -> InternalStatus:
@@ -217,16 +220,14 @@ class WebClient(Client, LazyLogging):
         Returns:
             InternalStatus: current internal (web) service status
         """
-        try:
+        with self.__execute_request():
             response = self.__session.get(self._status_url)
             response.raise_for_status()
 
             status_json = response.json()
             return InternalStatus.from_json(status_json)
-        except requests.HTTPError as e:
-            self.logger.exception("could not get web service status: %s", exception_response_text(e))
-        except Exception:
-            self.logger.exception("could not get web service status")
+
+        # noinspection PyUnreachableCode
         return InternalStatus(status=BuildStatus())
 
     def logs(self, package_base: str, record: logging.LogRecord) -> None:
@@ -254,13 +255,9 @@ class WebClient(Client, LazyLogging):
         Args:
             package_base(str): basename to remove
         """
-        try:
+        with self.__execute_request():
             response = self.__session.delete(self._package_url(package_base))
             response.raise_for_status()
-        except requests.HTTPError as e:
-            self.logger.exception("could not delete %s: %s", package_base, exception_response_text(e))
-        except Exception:
-            self.logger.exception("could not delete %s", package_base)
 
     def update(self, package_base: str, status: BuildStatusEnum) -> None:
         """
@@ -272,13 +269,9 @@ class WebClient(Client, LazyLogging):
         """
         payload = {"status": status.value}
 
-        try:
+        with self.__execute_request():
             response = self.__session.post(self._package_url(package_base), json=payload)
             response.raise_for_status()
-        except requests.HTTPError as e:
-            self.logger.exception("could not update %s: %s", package_base, exception_response_text(e))
-        except Exception:
-            self.logger.exception("could not update %s", package_base)
 
     def update_self(self, status: BuildStatusEnum) -> None:
         """
@@ -289,10 +282,6 @@ class WebClient(Client, LazyLogging):
         """
         payload = {"status": status.value}
 
-        try:
+        with self.__execute_request():
             response = self.__session.post(self._status_url, json=payload)
             response.raise_for_status()
-        except requests.HTTPError as e:
-            self.logger.exception("could not update service status: %s", exception_response_text(e))
-        except Exception:
-            self.logger.exception("could not update service status")

src/ahriman/application/application/application_packages.py
@@ -93,7 +93,8 @@ class ApplicationPackages(ApplicationProperties):
             source(str): remote URL of the package archive
         """
         dst = self.repository.paths.packages / Path(source).name  # URL is path, is not it?
-        response = requests.get(source, stream=True, timeout=None)  # timeout=None to suppress pylint warns
+        # timeout=None to suppress pylint warns. Also suppress bandit warnings
+        response = requests.get(source, stream=True, timeout=None)  # nosec
         response.raise_for_status()
 
         with dst.open("wb") as local_file:

tests/ahriman/core/auth/test_oauth.py
@@ -21,7 +21,7 @@ def test_get_provider() -> None:
     """
     assert OAuth.get_provider("OAuth2Client") == aioauth_client.OAuth2Client
     assert OAuth.get_provider("GoogleClient") == aioauth_client.GoogleClient
-    assert OAuth.get_provider("GoogleClient") == aioauth_client.GoogleClient
+    assert OAuth.get_provider("GithubClient") == aioauth_client.GithubClient
 
 
 def test_get_provider_not_a_type() -> None:
@@ -29,7 +29,7 @@ def test_get_provider_not_a_type() -> None:
     must raise an exception if attribute is not a type
     """
     with pytest.raises(OptionError):
-        OAuth.get_provider("__version__")
+        OAuth.get_provider("RANDOM")
 
 
 def test_get_provider_invalid_type() -> None:

commit be6cf5b8af752c806fdeeb2b33981cecaab25dae

fix case where it was impossible to create session if user and password are required
Issue appears if auth is enabled and unix socket is disabled and caused
by d81e91e1174a8854123c4ae26b6152fb3e24682d

src/ahriman/core/status/web_client.py
@@ -102,15 +102,22 @@ class WebClient(Client, LazyLogging):
         return address, False
 
     @contextlib.contextmanager
-    def __get_session(self) -> Generator[requests.Session, None, None]:
+    def __get_session(self, session: requests.Session | None = None) -> Generator[requests.Session, None, None]:
         """
         execute request and handle exceptions
+
+        Args:
+            session(requests.Session | None, optional): session to be used or stored instance property otherwise
+                (Default value = None)
 
         Yields:
             requests.Session: session for requests
         """
         try:
-            yield self.__session
+            if session is not None:
+                yield session  # use session from arguments
+            else:
+                yield self.__session  # use instance generated session
         except requests.RequestException as e:
             if self.suppress_errors:
                 return
@@ -136,13 +143,16 @@ class WebClient(Client, LazyLogging):
             return session
 
         session = requests.Session()
-        self._login()
+        self._login(session)
 
         return session
 
-    def _login(self) -> None:
+    def _login(self, session: requests.Session) -> None:
         """
         process login to the service
+
+        Args:
+            session(requests.Session): request session to login
         """
         if self.user is None:
             return  # no auth configured
@@ -152,7 +162,7 @@ class WebClient(Client, LazyLogging):
             "password": self.user.password
         }
 
-        with self.__get_session() as session:
+        with self.__get_session(session):
             response = session.post(self._login_url, json=payload)
             response.raise_for_status()
 

tests/ahriman/core/status/test_web_client.py
@@ -55,7 +55,7 @@ def test_create_session(web_client: WebClient, mocker: MockerFixture) -> None:
     session = web_client._create_session(use_unix_socket=False)
     assert isinstance(session, requests.Session)
     assert not isinstance(session, requests_unixsocket.Session)
-    login_mock.assert_called_once_with()
+    login_mock.assert_called_once_with(pytest.helpers.anyvar(int))
 
 
 def test_create_session_unix_socket(web_client: WebClient, mocker: MockerFixture) -> None:
@@ -80,7 +80,7 @@ def test_login(web_client: WebClient, user: User, mocker: MockerFixture) -> None
         "password": user.password
     }
 
-    web_client._login()
+    web_client._login(requests.Session())
     requests_mock.assert_called_once_with(pytest.helpers.anyvar(str, True), json=payload)
 
 
@@ -90,7 +90,7 @@ def test_login_failed(web_client: WebClient, user: User, mocker: MockerFixture) 
     """
     web_client.user = user
     mocker.patch("requests.Session.post", side_effect=Exception())
-    web_client._login()
+    web_client._login(requests.Session())
 
 
 def test_login_failed_http_error(web_client: WebClient, user: User, mocker: MockerFixture) -> None:
@@ -99,7 +99,7 @@ def test_login_failed_http_error(web_client: WebClient, user: User, mocker: Mock
     """
     web_client.user = user
     mocker.patch("requests.Session.post", side_effect=requests.exceptions.HTTPError())
-    web_client._login()
+    web_client._login(requests.Session())
 
 
 def test_login_skip(web_client: WebClient, mocker: MockerFixture) -> None:
@@ -107,7 +107,7 @@ def test_login_skip(web_client: WebClient, mocker: MockerFixture) -> None:
     must skip login if no user set
     """
     requests_mock = mocker.patch("requests.Session.post")
-    web_client._login()
+    web_client._login(requests.Session())
     requests_mock.assert_not_called()
 
 