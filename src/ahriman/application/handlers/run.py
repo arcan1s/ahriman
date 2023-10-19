16ad96d8c643efbaf7b62b2e5d0171ba77caeadd
Evgenii Alekseev <i@arcanis.me>
2023-10-19 04:34:49 +0300

    feat: add ability to run multiple commands on success

---
+++ src/ahriman/application/handlers/run.py
@@ -0,0 +1,66 @@
+#
+# Copyright (c) 2021-2023 ahriman team.
+#
+# This file is part of ahriman
+# (see https://github.com/arcan1s/ahriman).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import argparse
+import shlex
+
+from ahriman.application.handlers import Handler
+from ahriman.core.configuration import Configuration
+from ahriman.models.repository_id import RepositoryId
+
+
+class Run(Handler):
+    """
+    multicommand handler
+    """
+
+    ALLOW_MULTI_ARCHITECTURE_RUN = False  # system-wide action
+
+    @classmethod
+    def run(cls, args: argparse.Namespace, repository_id: RepositoryId, configuration: Configuration, *,
+            report: bool) -> None:
+        """
+        callback for command line
+
+        Args:
+            args(argparse.Namespace): command line args
+            repository_id(RepositoryId): repository unique identifier
+            configuration(Configuration): configuration instance
+            report(bool): force enable or disable reporting
+        """
+        parser = args.parser()
+        for command in args.command:
+            status = Run.run_command(shlex.split(command), parser)
+            Run.check_if_empty(True, not status)
+
+    @staticmethod
+    def run_command(command: list[str], parser: argparse.ArgumentParser) -> bool:
+        """
+        run command specified by the argument
+
+        Args:
+            command(list[str]): command to run
+            parser(argparse.ArgumentParser): generated argument parser
+
+        Returns:
+            bool: status of the command
+        """
+        args = parser.parse_args(command)
+        handler: Handler = args.handler
+        return handler.execute(args) == 0
---
+++ tests/ahriman/application/handlers/test_handler_run.py
@@ -0,0 +1,66 @@
+import argparse
+import pytest
+
+from pytest_mock import MockerFixture
+
+from ahriman.application.ahriman import _parser
+from ahriman.application.handlers import Run
+from ahriman.core.configuration import Configuration
+from ahriman.core.exceptions import ExitCode
+
+
+def _default_args(args: argparse.Namespace) -> argparse.Namespace:
+    """
+    default arguments for these test cases
+
+    Args:
+        args(argparse.Namespace): command line arguments fixture
+
+    Returns:
+        argparse.Namespace: generated arguments for these test cases
+    """
+    args.command = ["help"]
+    args.parser = _parser
+    return args
+
+
+def test_run(args: argparse.Namespace, configuration: Configuration, mocker: MockerFixture) -> None:
+    """
+    must run command
+    """
+    args = _default_args(args)
+    application_mock = mocker.patch("ahriman.application.handlers.Run.run_command")
+
+    _, repository_id = configuration.check_loaded()
+    Run.run(args, repository_id, configuration, report=False)
+    application_mock.assert_called_once_with(["help"], pytest.helpers.anyvar(int))
+
+
+def test_run_failed(args: argparse.Namespace, configuration: Configuration, mocker: MockerFixture) -> None:
+    """
+    must run commands until success
+    """
+    args = _default_args(args)
+    args.command = ["help", "config"]
+    application_mock = mocker.patch("ahriman.application.handlers.Run.run_command", return_value=False)
+
+    _, repository_id = configuration.check_loaded()
+    with pytest.raises(ExitCode):
+        Run.run(args, repository_id, configuration, report=False)
+    application_mock.assert_called_once_with(["help"], pytest.helpers.anyvar(int))
+
+
+def test_run_command(mocker: MockerFixture) -> None:
+    """
+    must correctly run external command
+    """
+    execute_mock = mocker.patch("ahriman.application.handlers.Help.execute")
+    Run.run_command(["help"], _parser())
+    execute_mock.assert_called_once_with(pytest.helpers.anyvar(int))
+
+
+def test_disallow_multi_architecture_run() -> None:
+    """
+    must not allow multi architecture run
+    """
+    assert not Run.ALLOW_MULTI_ARCHITECTURE_RUN
