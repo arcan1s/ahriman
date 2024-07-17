4f5166ff2568b0f6a0ebac02483ae7f95fdca4d8
Evgenii Alekseev <i@arcanis.me>
2024-07-17 17:58:24 +0300

    feat: improve lock mechanisms
    * improve lock mechanisms
    
    * use /run/ahriman for sockett
    
    * better water

--- tests/ahriman/models/test_waiter.py
+++ tests/ahriman/models/test_waiter.py
@@ -1,6 +1,36 @@
+import pytest
 import time
 
-from ahriman.models.waiter import Waiter
+from ahriman.models.waiter import Waiter, WaiterResult, WaiterTaskFinished, WaiterTimedOut
+
+
+def test_result_to_float() -> None:
+    """
+    must convert waiter result to float
+    """
+    assert float(WaiterResult(4.2)) == 4.2
+
+
+def test_result_not_implemented() -> None:
+    """
+    must raise NotImplementedError for abstract class
+    """
+    with pytest.raises(NotImplementedError):
+        assert bool(WaiterResult(4.2))
+
+
+def test_result_success_to_bool() -> None:
+    """
+    must convert success waiter result to bool
+    """
+    assert bool(WaiterTaskFinished(4.2))
+
+
+def test_result_failure_to_bool() -> None:
+    """
+    must convert failure waiter result to bool
+    """
+    assert not bool(WaiterTimedOut(4.2))
 
 
 def test_is_timed_out() -> None:
@@ -22,8 +52,26 @@ def test_is_timed_out_infinite() -> None:
 
 def test_wait() -> None:
     """
-    must wait until file will disappear
+    must wait for success result
     """
     results = iter([True, False])
-    waiter = Waiter(1, interval=1)
-    assert waiter.wait(lambda: next(results)) > 0
+    waiter = Waiter(1, interval=0.1)
+    assert float(waiter.wait(lambda: next(results))) > 0
+
+
+def test_wait_timeout() -> None:
+    """
+    must return WaiterTimedOut on timeout
+    """
+    results = iter([True, False])
+    waiter = Waiter(-1, interval=0.1)
+    assert isinstance(waiter.wait(lambda: next(results)), WaiterTimedOut)
+
+
+def test_wait_success() -> None:
+    """
+    must return WaiterTaskFinished on success
+    """
+    results = iter([True, False])
+    waiter = Waiter(1, interval=0.1)
+    assert isinstance(waiter.wait(lambda: next(results)), WaiterTaskFinished)
