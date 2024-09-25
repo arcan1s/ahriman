528d7ce3981fb6009a10bbb73a63d5863a47127b
Evgenii Alekseev <i@arcanis.me>
2024-09-25 03:46:33 +0300

    feat: suppress info log during vcs version calculation

--- tests/ahriman/core/log/test_lazy_logging.py
+++ tests/ahriman/core/log/test_lazy_logging.py
@@ -2,12 +2,24 @@ import logging
 import pytest
 
 from pytest_mock import MockerFixture
+from unittest.mock import call as MockCall
 
 from ahriman.core.alpm.repo import Repo
 from ahriman.core.build_tools.task import Task
 from ahriman.core.database import SQLite
 from ahriman.models.log_record_id import LogRecordId
 from ahriman.models.package import Package
+
+
+def test_logger(database: SQLite, repo: Repo) -> None:
+    """
+    must set logger attribute
+    """
+    assert database.logger
+    assert database.logger.name == "sql"
+
+    assert repo.logger
+    assert repo.logger.name == "ahriman.core.alpm.repo.Repo"
 
 
 def test_logger_name(database: SQLite, repo: Repo, task_ahriman: Task) -> None:
@@ -77,12 +89,11 @@ def test_in_package_context_failed(database: SQLite, package_ahriman: Package, m
     reset_mock.assert_called_once_with()
 
 
-def test_logger(database: SQLite, repo: Repo) -> None:
+def test_suppress_logging(database: SQLite, mocker: MockerFixture) -> None:
     """
-    must set logger attribute
+    must temporary disable log messages
     """
-    assert database.logger
-    assert database.logger.name == "sql"
-
-    assert repo.logger
-    assert repo.logger.name == "ahriman.core.alpm.repo.Repo"
+    disable_mock = mocker.patch("ahriman.core.log.lazy_logging.logging.disable")
+    with database.suppress_logging():
+        pass
+    disable_mock.assert_has_calls([MockCall(logging.WARNING), MockCall(logging.NOTSET)])
