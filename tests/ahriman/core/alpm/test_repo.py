f3fef1daf52109c4d53fad270008542fa8e5145a
Evgenii Alekseev <i@arcanis.me>
2025-08-14 13:32:55 +0300

    add archive trigger

--- tests/ahriman/core/alpm/test_repo.py
+++ tests/ahriman/core/alpm/test_repo.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from pytest_mock import MockerFixture
 
 from ahriman.core.alpm.repo import Repo
+from ahriman.models.package import Package
 from ahriman.models.repository_paths import RepositoryPaths
 
 
@@ -56,21 +57,37 @@ def test_repo_init(repo: Repo, mocker: MockerFixture) -> None:
     assert check_output_mock.call_args[0][0] == "repo-add"
 
 
-def test_repo_remove(repo: Repo, mocker: MockerFixture) -> None:
+def test_repo_remove(repo: Repo, package_ahriman: Package, mocker: MockerFixture) -> None:
     """
-    must call repo-remove on package addition
+    must call repo-remove on package removal
     """
+    filepath = package_ahriman.packages[package_ahriman.base].filepath
     mocker.patch("pathlib.Path.glob", return_value=[])
     check_output_mock = mocker.patch("ahriman.core.alpm.repo.check_output")
 
-    repo.remove("package", Path("package.pkg.tar.xz"))
+    repo.remove(package_ahriman.base, filepath)
     check_output_mock.assert_called_once()  # it will be checked later
     assert check_output_mock.call_args[0][0] == "repo-remove"
+    assert package_ahriman.base in check_output_mock.call_args[0]
+
+
+def test_repo_remove_guess_package(repo: Repo, package_ahriman: Package, mocker: MockerFixture) -> None:
+    """
+    must call repo-remove on package removal if no package name set
+    """
+    filepath = package_ahriman.packages[package_ahriman.base].filepath
+    mocker.patch("pathlib.Path.glob", return_value=[])
+    check_output_mock = mocker.patch("ahriman.core.alpm.repo.check_output")
+
+    repo.remove(None, filepath)
+    check_output_mock.assert_called_once()  # it will be checked later
+    assert check_output_mock.call_args[0][0] == "repo-remove"
+    assert package_ahriman.base in check_output_mock.call_args[0]
 
 
 def test_repo_remove_fail_no_file(repo: Repo, mocker: MockerFixture) -> None:
     """
-    must fail on missing file
+    must fail removal on missing file
     """
     mocker.patch("pathlib.Path.glob", return_value=[Path("package.pkg.tar.xz")])
     mocker.patch("pathlib.Path.unlink", side_effect=FileNotFoundError)
