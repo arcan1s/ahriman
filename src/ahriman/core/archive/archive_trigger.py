9f25e7a6ef9df1240da477fe7f4124d5b4a150c7
Evgenii Alekseev <i@arcanis.me>
2025-08-14 13:32:55 +0300

    add archive trigger

--- src/ahriman/core/archive/archive_trigger.py
+++ src/ahriman/core/archive/archive_trigger.py
@@ -17,21 +17,23 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-from pathlib import Path
-
+from ahriman.core import context
+from ahriman.core.archive.archive_tree import ArchiveTree
 from ahriman.core.configuration import Configuration
+from ahriman.core.sign.gpg import GPG
 from ahriman.core.triggers import Trigger
 from ahriman.models.package import Package
 from ahriman.models.repository_id import RepositoryId
 from ahriman.models.result import Result
 
 
-class ArchiveRotationTrigger(Trigger):
+class ArchiveTrigger(Trigger):
     """
     archive repository extension
 
     Attributes:
         paths(RepositoryPaths): repository paths instance
+        tree(ArchiveTree): archive tree wrapper
     """
 
     def __init__(self, repository_id: RepositoryId, configuration: Configuration) -> None:
@@ -44,9 +46,8 @@ class ArchiveRotationTrigger(Trigger):
 
         self.paths = configuration.repository_paths
 
-    @property
-    def repos_path(self) -> Path:
-        return self.paths.archive / "repos"
+        ctx = context.get()
+        self.tree = ArchiveTree(self.paths, ctx.get(GPG).repository_sign_args)
 
     def on_result(self, result: Result, packages: list[Package]) -> None:
         """
@@ -56,10 +57,16 @@ class ArchiveRotationTrigger(Trigger):
             result(Result): build result
             packages(list[Package]): list of all available packages
         """
+        self.tree.symlinks_create(packages)
 
     def on_start(self) -> None:
         """
         trigger action which will be called at the start of the application
         """
-        with self.paths.preserve_owner(self.repos_path):
-            self.repos_path.mkdir(mode=0o755, exist_ok=True)
+        self.tree.tree_create()
+
+    def on_stop(self) -> None:
+        """
+        trigger action which will be called before the stop of the application
+        """
+        self.tree.symlinks_fix()
