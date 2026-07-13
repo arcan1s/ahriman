2be82611bf5bfb47b48620fb85beb754774c80a1
Evgenii Alekseev <i@arcanis.me>
2026-07-13 13:01:54 +0300

    generic fixtures

--- ahriman-web/tests/conftest.py
+++ ahriman-web/tests/conftest.py
@@ -1,11 +1,8 @@
-# pylint: disable=wildcard-import,unused-wildcard-import
-from fixtures import *
-import sys
 from pathlib import Path
 
 import pytest
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "tests"))
+from fixtures import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 
 @pytest.fixture
